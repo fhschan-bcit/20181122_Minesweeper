0d20e2e5d7f8a4961384b27043a1c84169042819
Francis Chan <hchan154@my.bcit.ca>
2018-11-27 13:40:02 -0800

    Continuing minesweeper creation

--- js/board.js
+++ js/board.js
@@ -1,71 +1,76 @@
-var board = [];
-var WIDTH = HEIGHT = 4;
-var MINES = 5;
-var INIT_CONTENT = 0;
-var INIT_MINE = -10;
+let board = new Array(0);
+let WIDTH = HEIGHT = 9;
+let MINES = 9;
+let INIT_CONTENT = 0;
+let INIT_MINE = -10;
 
 function create_board() {
-    for (row = 0; row <= HEIGHT; row++) {
+    for (let row = 0; row <= HEIGHT; row++) {
         board.push([]);
-        for (column = 0; column <= WIDTH; column++) {
+        for (let column = 0; column <= WIDTH; column++) {
             board[row].push(INIT_CONTENT);
         }
     }
     console.log('Board created...', board);
 
-    for (random_mine = 0; random_mine < MINES; random_mine++) {
-        random_width = Math.round(Math.random() * WIDTH);
-        random_height = Math.round(Math.random() * HEIGHT);
-        console.log('Random mine x loc', random_width);
-        console.log('Random mine y loc', random_height);
+    for (let random_mine = 0; random_mine <= MINES; random_mine++) {
+        let random_width = Math.round(Math.random() * (board.length-1));
+        let random_height = Math.round(Math.random() * (board.length-1));
+        console.log('Random mine x loc', random_width, 'Random mine y loc', random_height);
         board[random_height][random_width] = INIT_MINE;
     }
 
     set_mine_hints();
-
-    function set_mine_hints() {
-        for (row = 0; row <= HEIGHT; row++) {
-            for (column = 0; column <= WIDTH; column++) {
-                content = board[row][column];
-                if (content < 0) {
-                    check_mine_row(row, column)
-                }
-            }
-        }
-    }
-
-    function check_mine_row(row, column) {
-        if (row == 0) {
-            board[row + 1][column] += 1;
-            check_mine_column(row, column)
-        }
-        else if (row == HEIGHT) {
-            board[row - 1][column] += 1;
-            check_mine_column(row, column);
-        }
-        else {
-            board[row + 1][column] += 1;
-            board[row - 1][column] += 1;
-            check_mine_column(row, column);
-        }
-    }
-
-    function check_mine_column(row, column) {
-        if (column == 0) {
-            board[row][column + 1] += 1;
-
-        }
-        else if (column == WIDTH) {
-            board[row][column - 1] += 1;
-        }
-        else {
-            board[row][column + 1] += 1;
-            board[row][column - 1] += 1;
-        }
-    }
     return board;
 }
 
+function set_mine_hints() {
+    for (let row = 0; row <= HEIGHT; row++) {
+        for (let column = 0; column <= WIDTH; column++) {
+            let content = board[row][column];
+            if (content < 0) {
+                check_mine_row(row, column)
+            }
+        }
+    }
+}
 
+function check_mine_row(row, column) {
+    // Checks and see if the neighbouring content is empty or not. Also
+    // checks if the neighbouring content is a mine or not.
+    
+    // Checking the board parameters
+    if (row == 0) {
+        // Checks if row is at the first index
+        board[row + 1][column] += 1;
+        check_mine_column(row, column)
+        check_mine_column(row+1, column);
+    }
+    else if (row == HEIGHT) {
+        // Checks if row is at the last index
+        board[row - 1][column] += 1;
+        check_mine_column(row, column);
+        check_mine_column(row-1, column);
+    }
+    else {
+        board[row + 1][column] += 1;
+        board[row - 1][column] += 1;
+        check_mine_column(row, column);
+        check_mine_column(row+1, column);
+        check_mine_column(row-1, column);
+    }
+}
 
-create_board();
+function check_mine_column(row, column) {
+    if (column == 0) {
+        board[row][column + 1] += 1;
+
+    }
+    else if (column == WIDTH) {
+        board[row][column - 1] += 1;
+    }
+    else {
+        board[row][column + 1] += 1;
+        board[row][column - 1] += 1;
+    }
+}
--- js/handler.js
+++ js/handler.js
@@ -1,1 +1,145 @@
-canvas
+let table = document.getElementById('h-board');
+let array_board = create_board();
+let BOARD_SIZE = array_board.length;
+let image_board = new Array(0);
+
+
+console.log('game board:', array_board);
+console.log('image board', image_board);
+console.log(BOARD_SIZE);
+store_img();
+print_board();
+
+
+function store_img() {
+    for (let x = 0; x < BOARD_SIZE; x++) {
+        image_board.push([]);
+        for (let y = 0; y < BOARD_SIZE; y++) {
+            let cell_content = insert_content(x, y);
+            image_board[x].push(cell_content);
+        }
+    }
+}
+
+function print_board(){
+    table.style.border = 'black';
+        for (let x = 0; x < BOARD_SIZE; x++){
+            let row = table.insertRow(x);
+            for(let y = 0; y < BOARD_SIZE; y++){
+                let cell = row.insertCell(y);
+                // Append image object here to cell.
+                cell.appendChild(image_board[x][y]);
+            }
+        }
+}
+
+function insert_content(x, y) {
+    let img = document.createElement('IMG');
+    img.style.maxHeight = '50px';
+    img.style.maxWidth = '50px';
+    img.style.height = '100%';
+    img.style.width = '100%';
+    img.src = './images/Box_Grey.ico';
+    if (array_board[x][y] < 0) {
+        img.onclick = function () {
+            img.src = './images/mine.png';
+            console.log('game over!');
+        }
+    }
+    else if (array_board[x][y] == 0) {
+        img.onclick = function () {
+            logic_reveal(x, y);
+            img.src = './images/0.png';
+        }
+    }
+    else if (array_board[x][y] == 1) {
+        img.onclick = function () {
+            img.src = './images/1.png';
+        }
+    }
+    else if (array_board[x][y] == 2) {
+        img.onclick = function () {
+            img.src = './images/2.png';
+        }
+    }
+    else if (array_board[x][y] == 3) {
+        img.onclick = function () {
+            img.src = './images/3.png';
+        }
+    }
+    else {
+        img.onclick = function () {
+            img.src = './images/square.jpeg';
+        }
+    }
+    return img;
+}
+
+function logic_reveal(x, y){
+
+    // Reveals horizontally and vertically at location x,y
+    reveal_multiple(x, y);
+
+    // Checks for boundary case of x+1 > board size
+    if (x+1 > BOARD_SIZE){
+        // Checks for boundary case of y+1 > board size
+        if (y+1 > BOARD_SIZE){
+            reveal_multiple(x-1, y-1);
+        }
+        // Checks for boundary case of y-1 > board size
+        else if (y-1 < 0){
+            reveal_multiple(x-1, y+1);
+        }
+    }
+    // Checks for boundary case of x-1 < 0
+    else if (x -1 < BOARD_SIZE){
+        if (y+1 > BOARD_SIZE){
+            reveal_multiple(x+1, y-1);
+        }
+        else if (y-1 < 0){
+            reveal_multiple(x+1, y+1);
+        }
+    }
+}
+
+function reveal_multiple(x, y){
+    console.log('revealing...');
+    console.log(array_board);
+
+    let check_y = y;
+    let check_x = x;
+
+    while (check_x < BOARD_SIZE && board[check_x][check_y] == 0){
+        if (array_board[check_x][check_y] == 0) {
+            image_board[check_x][check_y].src = './images/0.png';
+        }
+        check_x += 1;
+    }
+
+    check_x = x;
+
+    while (check_x >= 0 && board[check_x][check_y] == 0){
+        if (array_board[check_x][check_y] == 0) {
+            image_board[check_x][check_y].src = './images/0.png';
+        }
+        check_x -= 1;
+    }
+
+    check_x = x;
+
+    while (check_y < BOARD_SIZE && board[check_x][check_y] == 0){
+        if (array_board[check_x][check_y] == 0) {
+            image_board[check_x][check_y].src = './images/0.png';
+        }
+        check_y += 1;
+    }
+
+    check_y = y;
+
+    while (check_y >= 0 && board[check_x][check_y] == 0){
+        if (array_board[check_x][check_y] == 0) {
+            image_board[check_x][check_y].src = './images/0.png';
+        }
+        check_y -= 1;
+    }
+}
